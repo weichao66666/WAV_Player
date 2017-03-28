e86a64c782336380530879bae351dc28088dc160
weichao66666 <weichao66666@gmail.com>
2017-03-28 13:37:57 +0800

    Initial commit.

---
+++ app/src/main/java/io/weichao/wav_player/MainActivity.java
@@ -0,0 +1,51 @@
+package io.weichao.wav_player;
+
+import android.app.AlertDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    static {
+        System.loadLibrary("WAVPlayer");
+    }
+
+    private native void play(String fileName) throws IOException;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        File file = new File(Environment.getExternalStorageDirectory(),
+                "8k16bitpcm.wav");
+        PlayTask playTask = new PlayTask();
+        playTask.execute(file.getAbsolutePath());
+    }
+
+    private class PlayTask extends AsyncTask<String, Void, Exception> {
+        protected Exception doInBackground(String... file) {
+            Exception result = null;
+
+            try {
+                play(file[0]);
+            } catch (IOException ex) {
+                result = ex;
+            }
+
+            return result;
+        }
+
+        protected void onPostExecute(Exception ex) {
+            if (ex != null) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Error Occurred")
+                        .setMessage(ex.getMessage()).show();
+            }
+        }
+    }
+}
